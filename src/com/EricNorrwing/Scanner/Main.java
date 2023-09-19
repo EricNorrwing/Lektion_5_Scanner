ebbfe058aafe6d180c610d5db306278dcdc67cc5
EricNorrwing <Eric.Norrwing@gmail.com>
2023-09-19 10:39:04 +0200

    Startup

---
+++ src/com/EricNorrwing/Scanner/Main.java
@@ -0,0 +1,14 @@
+package com.EricNorrwing.Scanner;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("App is running");
+
+
+        
+    }
+}
