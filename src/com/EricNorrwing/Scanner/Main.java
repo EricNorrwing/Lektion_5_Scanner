a804494407b75548c90de5e5f433f495dd1a5c6d
EricNorrwing <Eric.Norrwing@gmail.com>
2023-09-19 10:42:28 +0200

    Added dev branch

--- src/com/EricNorrwing/Scanner/Main.java
+++ src/com/EricNorrwing/Scanner/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        //added new branch
         System.out.println("App is running");
 
 
-        
+
     }
 }
